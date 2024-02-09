6bd95af410c4f314cd4494a6f90bb0741a328547
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2024-02-09 13:16:26 +0100

    Google & Github Providers added!

--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,6 @@
 import bcrypt from "bcryptjs";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 import type { NextAuthConfig } from "next-auth";
 import credentials from "next-auth/providers/credentials";
 import { LoginSchema } from "./schemas";
@@ -6,6 +8,14 @@ import { getUserByEmail } from "./data/user";
 
 export default {
   providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     credentials({
       async authorize(credentials, request) {
         const validatedFields = LoginSchema.safeParse(credentials);
--- components/auth/Social.tsx
+++ components/auth/Social.tsx
@@ -1,16 +1,35 @@
+"use client";
+
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { Button } from "../ui/button";
+import { DEFAUTL_LOGIN_REDIRECT } from "@/route";
+import { signIn } from "next-auth/react";
 
 const Social = () => {
+  const onClickHandler = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: DEFAUTL_LOGIN_REDIRECT,
+    });
+  };
   return (
     <div className="w-full flex items-center gap-x-2">
-      <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
+      <Button
+        size="lg"
+        className="w-full"
+        variant="outline"
+        onClick={() => onClickHandler("google")}
+      >
         <FcGoogle className="h-5 w-5" />
       </Button>
 
-      <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
+      <Button
+        size="lg"
+        className="w-full"
+        variant="outline"
+        onClick={() => onClickHandler("github")}
+      >
         <FaGithub className="h-5 w-5" />
       </Button>
     </div>
