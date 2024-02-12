564479d5fb2d26d5cbb739e20fa3d04740f167a8
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2024-02-12 18:02:46 +0100

    Problems have been fixed!

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,15 @@
 import authConfig from "./auth.config";
 import NextAuth from "next-auth";
-export const { auth } = NextAuth(authConfig);
+
 import {
   DEFAUTL_LOGIN_REDIRECT,
   publicRoutes,
   apiAuthPrefix,
   authRoutes,
 } from "./route";
+import { NextResponse } from "next/server";
+
+export const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
   const { nextUrl } = req;
@@ -16,19 +19,19 @@ export default auth((req) => {
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-  if (isApiRouteAuth) return null;
+  if (isApiRouteAuth) return NextResponse.next();
 
   if (isAuthRoute) {
     if (isLoggedIn)
       return Response.redirect(new URL(DEFAUTL_LOGIN_REDIRECT, nextUrl));
-    return null;
+    return NextResponse.next();
   }
 
   if (!isLoggedIn && !isPublicRoute) {
     return Response.redirect(new URL("/auth/login", nextUrl));
   }
 
-  return null;
+  return NextResponse.next();
 });
 
 // Optionally, don't invoke Middleware on some paths
