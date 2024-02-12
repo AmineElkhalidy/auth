98ee53e11a91dee85068126d51484a6f5d0e8176
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2024-02-12 15:27:31 +0100

    Added settings page!

--- components/UserInfo.tsx
+++ components/UserInfo.tsx
@@ -46,7 +46,9 @@ const UserInfo = ({ user, label }: UserInfoProps) => {
         <div className="flex items-center justify-between rounded-lg border p-3 shadow-sm">
           <p className="text-sm font-semibold">Two Factor Authentication:</p>
 
-          <Switch checked={user?.isTwoFactorEnabled} />
+          <Badge variant={user?.isTwoFactorEnabled ? "success" : "destructive"}>
+            {user?.isTwoFactorEnabled ? "ON" : "OFF"}
+          </Badge>
         </div>
       </CardContent>
     </Card>
